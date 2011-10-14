972eea625ac0a156077e20a3e098a73ae617f1a0
Majna <majnaggz@gmail.com>
2011-10-14 22:43:49 +0200

    Datetime values are now randomized. Updating docs and adding examples.#

--- README.md
+++ README.md
@@ -1,27 +1,101 @@
-# CakePHP app schema with sample data ready for baking
+# CakePHP app schema for Sample Blog Application
 
-This app can be used for benchmarking, testing, debugging etc. on CakePHP 1.3.x and CakePHP 2.x
+Useful when learning CakePHP, for benchmarks, tests, debugging and more.
 
-Quickly bake sample application with following tables and number of inserted rows:
+All you need is to copy schema.php to APP/Config/Schema and run cake console task.
 
-    'blogs'=>6, 'categories' =>50, 'comments'=>2000, 'languages'=>1,'posts'=>1000,
-    'posts_tags'=>2000, 'profiles'=>1000, 'tags'=>1000, 'users'=>1000
+Following tables (and number of inserted rows) will be created (can be configured in schema file):
 
+	blogs: 6, 
+	categories: 50, 
+	comments: 2000, 
+	languages: 1,
+	posts : 1000,
+	posts_tags : 2000,
+	tags : 1000, 
+	users : 1000
+
+It tries to match data values for name, date, email address, title, post etc.
 
 ## Usage
 
-1. Install fresh copy of CakePHP.
-2. Configure database in "app/config/database.php".
-3. Browse to homepage and confirm Cake install check (all green). Make sure "app/tmp" folder is writable (and subfolders).
-4. Copy provided schema.php to "app/config/schema" folder.
-5. Configure CakePHP console.
-6. Run in console "cake schema create app". Wait for a minute until all records are inserted.
-7. Run in console "cake bake all". Repeat for all available models.
-8. All done, now browse to "/posts".
+1. Install fresh copy of CakePHP (1.3.x or 2.x)
+2. Configure database in "APP/Config/database.php".
+3. Browse to home page and confirm Cake setup checks.
+4. Copy provided schema.php to "APP/Config/Schema" folder.
+5. Configure and run CakePHP console: 
 
-For benchmarking, edit "app/config/core.php" and change "Configure::write('debug', 0);" to enable cache.
+    `cake schema create app` Wait for a minute until all records are inserted.
+    `cake bake all` Repeat for all available models.
 
-## Requirements
+All done, now browse to "/posts".
+
+If benchmarking, disable debug mode: edit `app/Config/core.php` and change `Configure::write('debug', 0);`
+
+
+## Example of few  records created in table 'users':
+
+	id: 1
+	name:		Eleifend Vivamus
+	email:		proin@example.com
+	password:		db6ff2ffe2df7b8cfc0d9542bdce27dc
+	phone:		4624-8435
+	mobile_phone:	2078-8235
+	address:		Sit orci amet
+	city:			Sociis
+	postcode:		8561
+	web:			http:/www.fermentum.com
+	created:		2011-05-15 22:01:01
+	modified:		2011-07-13 20:31:06
+
+	id: 2
+	name:		Risus Proin
+	email:		fringilla@example.com
+	password:		c3d32ae7ad6a5d450756a7d96b2a2a21
+	phone:		4624-8435
+	mobile_phone:	2078-8235
+	address:		Sit orci amet
+	city:			Morbi
+	postcode:		8561
+	web:			http:/www.fermentum.com
+	created:		2011-08-15 23:30:16
+	modified:		2011-09-05 09:31:26
+
+## Example of created records in table 'posts':
+
+It will assign few tags to each Post and pick a random category and blog.
+
+	id:			1
+	category_id:	48
+	title:			Quam nisl lacinia
+	description:	Tincidunt nisi nascetur mollis vitae vel mus quam mperdiet dis Vivamus libero.
+	body:		Urna tortor dapibus consectetur sit nisl interdum placerat ipsum pellentesque dictum tortor orci pulvinar 
+				qegestas varius fringilla at adipiscing parturient congue risus Donec aliquet velit mus consectetur consequat
+				 imperdiet eleifend mauris sed blandit lacus eleifend est Proin quis nisi mollis Cum sociis Donec vestibulum 
+				posuere sodales quam vitae ante magnis Maecenas tincidunt consectetur Phasellus sagittis sed Lorem tincidunt 
+				lacinia turpis elit Quisque congue congue libero amet congue sed urna vitae nisi ipsum tristique Integer montes leo.
+	created:		2011-07-15 02:12:36
+	modified:		2011-09-15 23:30:19
+	featured:		0
+	published:		1
+	blog_id:		4
+
+
+	id:			2
+	category_id:	36
+	title:			Tristique Aenean erat
+	description:	Aenean ipsum leo nibh posuere nascetur venenatis at malesuada vestibulum ligula euismod.
+	body:		Dictum imperdiet nisi Quisque Donec tristique sit malesuada diam Proin ligula sodales magnis sagittis
+				pulvinar at ante consectetur consequat venenatis ullamcorper quam orci sit amet congue blandit dignissim 
+				posuere vestibulum congue amet tortor penatibus Aenean congue placerat sollicitudin velit lacinia Morbi.
+	created:		2011-07-15 21:34:46
+	modified:		2011-09-05 10:11:06
+	featured:		1
+	published:		1
+	blog_id:		5
+
+
+### Requirements
 
     PHP version: PHP 5.2+
     CakePHP version: 1.3, 2.x
--- schema.php
+++ schema.php
@@ -1,17 +1,8 @@
 <?php
-/* App schema generated on: 2011-07-15 16:51:32 : 1310748692 */
-
+/*
+ * App schema for "Sample blog application"
+ */
 class AppSchema extends CakeSchema {
-
-	public $name = 'App';
-
-	public function before($event = array()) {
-		return true;
-	}
-
-	public function after($event = array()) {
-		
-	}
 
 	public $blogs = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
@@ -52,8 +43,8 @@ class AppSchema extends CakeSchema {
 		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 128, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'locale' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'active' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00'),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
@@ -63,8 +54,8 @@ class AppSchema extends CakeSchema {
 		'title' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'description' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'body' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00'),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'featured' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'published' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'blog_id' => array('type' => 'integer', 'null' => true, 'default' => '0', 'key' => 'index'),
@@ -81,21 +72,6 @@ class AppSchema extends CakeSchema {
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'post_id' => array('column' => 'post_id', 'unique' => 0), 'tag_id' => array('column' => 'tag_id', 'unique' => 0)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
-	public $profiles = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
-		'user_id' => array('type' => 'integer', 'null' => false, 'default' => '0', 'key' => 'unique'),
-		'phone' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 128, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'mobile_phone' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 128, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'address' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'city' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'postcode' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 32, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'web' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00'),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00'),
-		'public' => array('type' => 'boolean', 'null' => false, 'default' => '1'),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'user_id' => array('column' => 'user_id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
 	public $tags = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
 		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
@@ -103,8 +79,6 @@ class AppSchema extends CakeSchema {
 		'weight' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 2),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
-		'created_by' => array('type' => 'integer', 'null' => true, 'default' => NULL),
-		'modified_by' => array('type' => 'integer', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'slug' => array('column' => 'slug', 'unique' => 0)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
 	);
@@ -113,8 +87,14 @@ class AppSchema extends CakeSchema {
 		'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 32, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'email' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 128, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'password' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 40, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'phone' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 128, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'address' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'city' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 64, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'postcode' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 32, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'web' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'active' => array('type' => 'boolean', 'null' => false, 'default' => '1'),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
@@ -123,7 +103,17 @@ class AppSchema extends CakeSchema {
 	 *  Populate tables with lorem ispum text
 	 */
 	public function __destruct() {
-		$config = array('blogs' => 6, 'categories' => 50, 'comments' => 2000, 'languages' => 1, 'posts' => 1000, 'posts_tags' => 2000, 'profiles' => 1000, 'tags' => 1000, 'users' => 1000);
+		$config = array(
+			'blogs' => 6,
+			'categories' => 50,
+			'comments' => 2000,
+			'languages' => 1,
+			'posts' => 1000,
+			'posts_tags' => 2000,
+			'tags' => 1000,
+			'users' => 1000
+		);
+
 		foreach ($config as $table => $numRecords) {
 			$model = ClassRegistry::init($table);
 			$model->getDataSource()->cacheMethods = false;
@@ -131,103 +121,93 @@ class AppSchema extends CakeSchema {
 			for ($i = 1; $i <= $numRecords; $i++) {
 				$data = $this->{$table}($config);
 				if (is_array($data)) {
+					$data['created'] = date('Y-m-d H:i:s', strtotime('-'. rand(100, 9999) . ' minutes'));
+					$data['modified'] = date('Y-m-d H:i:s', strtotime('-'. rand(100, 9999) . ' minutes'));
 					$model->create();
 					$model->save($data, false);
 				}
 			}
-			unset($model);
-			ClassRegistry::removeObject($table);
 		}
 	}
 
-	private function blogs($config) {
+	protected function blogs($config) {
 		return array(
 			'user_id' => rand(1, $config['users']),
 			'title' => $this->lorem(3),
-			'description' => $this->lorem(15),
-			'notes' => $this->lorem(6)
+			'description' => $this->lorem(rand(15, 30)) . '.',
+			'notes' => $this->lorem(rand(2, 8))
 		);
 	}
 
-	private function categories() {
+	protected function categories() {
 		return array(
 			'name' => $this->lorem(2),
 			'slug' => $this->lorem(2),
-			'description' => $this->lorem(4)
+			'description' => $this->lorem(rand(4, 12)) . '.'
 		);
 	}
 
-	private function comments($config) {
+	protected function comments($config) {
 		return array(
 			'post_id' => rand(1, $config['posts']),
 			'user_id' => rand(1, $config['users']),
-			'title' => $this->lorem(3),
-			'comment' => $this->lorem(16)
+			'title' => $this->lorem(rand(2, 7)),
+			'comment' => $this->lorem(rand(3, 100)) . '.'
 		);
 	}
 
-	private function languages() {
+	protected function languages() {
 		return array(
 			'name' => 'english',
-			'locale' => 'en_EN'
+			'locale' => 'en_EN',
 		);
 	}
 
-	private function posts($config) {
+	protected function posts($config) {
 		return array(
 			'blog_id' => rand(1, $config['blogs']),
 			'category_id' => rand(1, $config['categories']),
-			'title' => $this->lorem(3),
-			'description' => $this->lorem(12),
-			'body' => $this->lorem(500),
+			'title' => $this->lorem(rand(4, 9)),
+			'description' => $this->lorem(rand(9, 22)) . '.',
+			'body' => $this->lorem(rand(200, 800)),
+			'published' => 1,
+			'featured' => rand(1, 0),
 		);
 	}
 
-	private function posts_tags($config) {
+	protected function posts_tags($config) {
 		static $i = 0;
 		$i++;
 		if ($i > $config['posts'] || $i > $config['tags']) {
 			return false;
 		}
-		
+
 		return array(
 			'post_id' => $i,
 			'tag_id' => $i,
 		);
 	}
 
-	private function profiles($config) {
-		static $i = 0;
-		$i++;
-		if ($i > $config['users']) {
-			return false;
-		}
-		
-		return array(
-			'user_id' => $i,
-			'address' => $this->lorem(3),
-			'city' => $this->lorem(1),
-			'phone' => rand(1000, 9000) . '-' . rand(1000, 9000),
-			'mobile_phone' => rand(1000, 9000) . '-' . rand(1000, 9000),
-			'postcode' => rand(1000, 9000),
-			'city' => $this->lorem(1),
-			'web' => 'www' . lcfirst($this->lorem(1)) . 'com',
-		);
-	}
-
-	private function tags() {
+	protected function tags() {
 		return array(
 			'name' => $this->lorem(1),
-			'slug' => $this->lorem(1),
+			'slug' => strtolower($this->lorem(1)),
 			'weight' => rand(1, 9),
 		);
 	}
 
-	private function users() {
+	protected function users() {
 		return array(
-			'name' => lcfirst($this->lorem(1)),
-			'email' => trim($this->lorem(1)) . '@example.com',
-			'password' => md5($this->lorem(1))
+			'name' => ucfirst($this->lorem(1)) . ' ' . ucfirst($this->lorem(1)),
+			'email' => lcfirst($this->lorem(1)) . '@' . lcfirst($this->lorem(1)) . 'com',
+			'password' => md5($this->lorem(1)),
+			'address' => $this->lorem(3),
+			'city' => $this->lorem(1),
+			'phone' => rand(1000, 9000) . '-' . rand(10000, 90000),
+			'mobile_phone' => rand(1000, 9000) . '-' . rand(10000, 90000),
+			'postcode' => rand(1000, 9000),
+			'city' => $this->lorem(1),
+			'web' => 'http://www.' . lcfirst($this->lorem(1)) . '.com',
 		);
 	}
 
@@ -236,20 +216,30 @@ class AppSchema extends CakeSchema {
 	 * @param integer $numWords
 	 * @return string "Lorem ipsum" words 
 	 */
-	private function lorem($numWords = 1) {
-		$words = "Lorem ipsum dolor sit amet consectetur adipiscing elit Vestibulum blandit interdum dui eget dignissim libero eleifend sit amet Vivamus in dolor sed tortor congue posuere Donec varius congue urna pulvinar ante imperdiet vel Mauris orci nisl egestas dapibus malesuada blandit nisi Cras ligula sit amet mauris dictum eleifend libero Proin erat est pellentesque auctor vitae tincidunt quis lacus Phasellus turpis nibh sollicitudin consequat quis aliquet Morbi vitae diam tortor Quisque tellus vestibulum sed congue sed venenatis at risus Integer consectetur lacinia diam tristique leo fringilla Donec fermentum congue congue Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus Donec mollis orci euismod blandit velit nisi tincidunt ipsum vitae consectetur Maecenas sagittis placerat sodales ullamcorper urna mperdiet ligula quam Aenean mollis";
+	protected function lorem($numWords = 1) {
+		$words = "Lorem ipsum dolor sit amet consectetur adipiscing elit Vestibulum blandit interdum dignissim "
+			. "libero eleifend sit amet Vivamus dolor sed tortor congue posuere Donec varius congue "
+			. "pulvinar ante imperdiet vel Mauris orci nisl egestas dapibus malesuada blandit nisi Cras "
+			. "ligula sit amet mauris dictum eleifend libero Proin pellentesque auctor vitae tincidunt quis "
+			. "lacus Phasellus turpis nibh sollicitudin consequat quis aliquet Morbi vitae diam tortor  "
+			. "Quisque tellus vestibulum sed congue sed venenatis at risus Integer consectetur lacinia "
+			. "diam tristique leo fringilla Donec fermentum congue congue Cum sociis natoque penatibus "
+			. "et magnis dis parturient montes nascetur ridiculus mus Donec mollis orci euismod blandit "
+			. "tincidunt vitae consectetur Maecenas sagittis placerat sodales ullamcorper  mperdiet ligula ";
 
-		$list = explode(' ', $words);
-		shuffle($list);
+		$list = shuffle(explode(' ', $words));
 		$out = array();
-
+		
 		for ($i = 1; $i <= $numWords; $i++) {
 			if (isset($list[$i])) {
 				$out[] = $list[$i];
 			}
 		}
 
-		return ucfirst(implode(' ', $out));
+		if ($numWords === 1) {
+			$out = trim($out);
+		}
+		return ucfirst(implode(' ', $out)) . '.';
 	}
 
 }
